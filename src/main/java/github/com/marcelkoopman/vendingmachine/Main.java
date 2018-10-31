bd01bb9dce2ad939397ff48f48ef3b79f3759c53
marcelkoopman <marcel.koopman@gmail.com>
2018-10-31 18:48:04 +0100

    Added some files

--- src/main/java/github/com/marcelkoopman/vendingmachine/Main.java
+++ src/main/java/github/com/marcelkoopman/vendingmachine/Main.java
@@ -1,4 +1,16 @@
-package github.com.marcelkoopman
+package github.com.marcelkoopman.vendingmachine;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Main {
+
+    private static final Logger LOGGER = LogManager.getLogger(Main.class.getName());
+
+
+    public static void main(String[] args) {
+        LOGGER.info("VendingMachine up");
+        LOGGER.info("VendingMachine down");
+    }
 }
