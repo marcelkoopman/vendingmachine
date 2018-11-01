94223c73f76db6f46a21cf736997182fa77579f4
marcelkoopman <marcel.koopman@gmail.com>
2018-11-01 19:46:41 +0100

    First initial ideas

--- src/main/java/github/com/marcelkoopman/vendingmachine/Main.java
+++ src/main/java/github/com/marcelkoopman/vendingmachine/Main.java
@@ -1,12 +1,16 @@
 package github.com.marcelkoopman.vendingmachine;
 
 
+import github.com.marcelkoopman.vendingmachine.api.Product;
 import github.com.marcelkoopman.vendingmachine.api.VendingMachine;
 import github.com.marcelkoopman.vendingmachine.impl.VendingMachineV1;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -14,8 +18,20 @@ public class Main {
 
     private static final VendingMachine vendingMachine = new VendingMachineV1();
 
+    private static final Map<String, String> commands = new TreeMap<>();
+
+    static {
+        commands.put("help", "Show this help text");
+        commands.put("select", "Select this product");
+        commands.put("price", "Show price of this product");
+        commands.put("list", "Show all available products");
+        commands.put("quit", "Quit application");
+    }
+
     public static void main(String[] args) {
         vendingMachine.boot();
+        LOGGER.info(getHelp());
+
         Scanner scanner = new Scanner(System.in);
         while (readFromConsole(scanner)) {
             //
@@ -32,6 +48,10 @@ public class Main {
         } else if ("quit".equals(line)) {
             LOGGER.info("Quitting...");
             continueReading = false;
+        } else if ("list".equals(line)) {
+            final Map<Product, Integer> products = vendingMachine.getAvailableProducts();
+            LOGGER.info("Available products: " + products.entrySet().stream().map(p -> p.getKey().getName()).collect(Collectors.joining(",")));
+            continueReading = true;
         } else {
             LOGGER.warn("Unrecognized command: " + line);
             continueReading = true;
@@ -39,11 +59,16 @@ public class Main {
         return continueReading;
     }
 
+    // Retrieve and show all products in a single list.
+    // Accept user input for selecting a product.
+    // Show the price for a selected product.
+    // Accept user input for paying for the selected product.
+    // Display the purchased product.
+
+
     private static String getHelp() {
         final StringBuilder builder = new StringBuilder();
-        builder.append("help = show this help");
-        builder.append(", ");
-        builder.append("quit = quit application");
+        builder.append(commands);
         return builder.toString();
     }
 }
