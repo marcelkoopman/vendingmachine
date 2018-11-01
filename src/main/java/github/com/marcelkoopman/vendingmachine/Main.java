289922b842f42f37ec66a8ad0b32a38492d2eb88
marcelkoopman <marcel.koopman@gmail.com>
2018-11-01 19:17:15 +0100

    First initial ideas

--- src/main/java/github/com/marcelkoopman/vendingmachine/Main.java
+++ src/main/java/github/com/marcelkoopman/vendingmachine/Main.java
@@ -6,6 +6,8 @@ import github.com.marcelkoopman.vendingmachine.impl.VendingMachineV1;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.Scanner;
+
 public class Main {
 
     private static final Logger LOGGER = LogManager.getLogger(Main.class.getName());
@@ -14,6 +16,34 @@ public class Main {
 
     public static void main(String[] args) {
         vendingMachine.boot();
+        Scanner scanner = new Scanner(System.in);
+        while (readFromConsole(scanner)) {
+            //
+        }
         vendingMachine.shutDown();
+    }
+
+    private static boolean readFromConsole(Scanner scanner) {
+        final String line = scanner.nextLine();
+        final boolean continueReading;
+        if ("help".equals(line)) {
+            LOGGER.info(getHelp());
+            continueReading = true;
+        } else if ("quit".equals(line)) {
+            LOGGER.info("Quitting...");
+            continueReading = false;
+        } else {
+            LOGGER.warn("Unrecognized command: " + line);
+            continueReading = true;
+        }
+        return continueReading;
+    }
+
+    private static String getHelp() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append("help = show this help");
+        builder.append(", ");
+        builder.append("quit = quit application");
+        return builder.toString();
     }
 }
